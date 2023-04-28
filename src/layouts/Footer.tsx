c11e679d9c03260be0b0ce5f833c72086a1841b5
Anesu Cain Mugomba <anesucain@gmail.com>
2023-04-28 19:28:56 +0200

    base color

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div>
+        <div className='social-icons'>
+        <i className="fa fa-github fa-xs"></i>
+
+<i className="fa fa-linkedin-square"></i>
+
+<i className="fa fa-twitter"></i>
+        </div>
+
+        <div className='navigation'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
