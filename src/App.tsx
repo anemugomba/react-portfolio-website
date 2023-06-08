0481ba1743364930828190595fe6734bbef11609
Anesu Cain Mugomba <anesucain@gmail.com>
2023-06-08 13:51:48 +0200

    removed unwanted pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ import {
   Route,
 } from "react-router-dom";
 import Home from './home/Home';
-import Portfolio from './portfolio/Portfolio';
-import Contact from './contact/Contact';
-import About from './about/About';
 
 function App() {
 
@@ -18,9 +15,6 @@ function App() {
       <Layout>
         <Routes>
           <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/portfolio' element={<Portfolio />}/>
-          <Route path='/contact' element={<Contact />}/>
         </Routes>
       </Layout>
     </Router>
