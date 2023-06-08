1805a10d2720d8932c9204f1ff09797eeb2f51c4
Anesu Cain Mugomba <anesucain@gmail.com>
2023-06-08 08:44:55 +0200

    added reirect to soial platforms on navbar

--- src/layouts/NavigationBar.tsx
+++ src/layouts/NavigationBar.tsx
@@ -14,17 +14,20 @@ function NavigationBar() {
     }
   }, [width])
 
+  const redirectSocial = (url: string) => {
+    window.open(url, '_blank')
+  }
 
   return (
     <div className='fixed'>
       <div className='navigation-container'>
         <div className={`topnav ${responsive ? 'responsive' : ''}`} id="myTopnav">
           <span className='social-icons'>
-            <i className="fa fa-github fa-xs"></i>
+            <i className="fa fa-github fa-xs" onClick={() => redirectSocial('https://github.com/anemugomba')}></i>
 
-            <i className="fa fa-linkedin-square"></i>
+            <i className="fa fa-linkedin-square" onClick={() => redirectSocial('https://www.linkedin.com/in/anesu-mugomba-a51a851a2/')}></i>
 
-            <i className="fa fa-twitter"></i>
+            <i className="fa fa-twitter" onClick={() => redirectSocial('https://twitter.com/mambowechidiki')}></i>
 
           </span>
 
